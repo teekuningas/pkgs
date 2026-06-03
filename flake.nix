d8f8c991498509bfe1754c61b4a3ae065a2d1ea7
teekuningas <erkkahe@gmail.com>
2026-06-03 07:02:07 +0000

    Clean up

--- flake.nix
+++ flake.nix
@@ -4,14 +4,12 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    poetry2nix = { url = "github:nix-community/poetry2nix"; inputs.nixpkgs.follows = "nixpkgs"; inputs.flake-utils.follows = "flake-utils"; };
   };
 
   outputs = { self, nixpkgs, flake-utils, ...}@inputs:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
-        poetry2nix = inputs.poetry2nix.lib.mkPoetry2Nix { inherit pkgs; };
       in {
         packages = {
           safepilot = pkgs.callPackage ./pkgs/safepilot { };
@@ -19,11 +17,6 @@
           safepilot-with-gemini = pkgs.callPackage ./pkgs/safepilot { withGemini = true; };
           safepilot-with-opencode = pkgs.callPackage ./pkgs/safepilot { withOpencode = true; };
           safepilot-full = pkgs.callPackage ./pkgs/safepilot { withCopilot = true; withGemini = true; withOpencode = true; };
-          ospeak = pkgs.callPackage ./pkgs/ospeak { python3Packages = pkgs.python312Packages; };
-          files-to-prompt = pkgs.callPackage ./pkgs/files-to-prompt { python3Packages = pkgs.python312Packages;};
-          llm-azure = pkgs.callPackage ./pkgs/llm-azure { python3Packages = pkgs.python312Packages;};
-          llm-echo = pkgs.callPackage ./pkgs/llm-echo { python3Packages = pkgs.python312Packages;};
-          llm-fragments-site-text = pkgs.callPackage ./pkgs/llm-fragments-site-text { python3Packages = pkgs.python312Packages;};
         };
       }
     );
